de5148133680e203a028b3acf602bae0c87755ec
Thariqalfa <zrav420@gmail.com>
2019-08-06 19:23:37 +0700

    zxing library qrcode

--- app/src/main/java/com/example/parkir/activities/HomeKangParkir.java
+++ app/src/main/java/com/example/parkir/activities/HomeKangParkir.java
@@ -1,7 +1,9 @@
 package com.example.parkir.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.parkir.R;
 
@@ -11,5 +13,10 @@ public class HomeKangParkir extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_kang_parkir);
+    }
+
+    public void myQrCode(View view){
+        Intent i = new Intent(HomeKangParkir.this, QrCode.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/parkir/activities/QrCode.java
@@ -0,0 +1,37 @@
+package com.example.parkir.activities;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.parkir.R;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+public class QrCode extends AppCompatActivity {
+    private ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr);
+
+        imageView = (ImageView) this.findViewById(R.id.imageView);
+
+        String text2Qr = "alfaben";
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(text2Qr, BarcodeFormat.QR_CODE,200,200);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            imageView.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+    }
+}
